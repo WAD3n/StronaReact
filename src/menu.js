903e9558dcfd0fa340142648638323c82b9edd29
WAD3n <gluchowski.hubert@gmail.com>
2024-01-23 16:00:34 +0100

    Poczatek

---
+++ src/menu.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+
+function menu() {
+    return (
+        <>
+            <nav className="menu">
+                <div className="menu-kontener">
+                    <Link to ='/' className="logo-menu">
+
+                    </Link>
+                </div>
+            </nav>
+        </>
+    )
+}
+export default  menu
